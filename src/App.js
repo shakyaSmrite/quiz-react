d62e057a1630656e494da008251c299d03ce0e14
Smriti Shakya <smriti.shakya@mavs.uta.edu>
2021-08-08 01:19:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import data from "./api/multipleChoice.json";
 
 function App() {
+  const copyData = [...data.options];
+  const [score, setScore] = useState(0);
+  const [ques, setQues] = useState(0);
+
+  //function call to change to next question
+  function handleNextClick(i) {
+    const nextQuestion = i + 1;
+    setQues(nextQuestion);
+  }
+
+  //call to move back to previous question
+  function handlePrevClick(i) {
+    const prevQuestion = i - 1;
+    setQues(prevQuestion);
+  }
+
+  function handlePrevDisabler(i) {
+    if (i === 0) return true;
+  }
+
+  function handleNextDisabler(i) {
+    if (i === copyData.length) return true;
+  }
+
+  //answercheck and score update
+  function handleCheckAns(id) {
+    id == copyData[ques].correctAnswerID
+      ? setScore(score + 1)
+      : setScore(score + 0);
+  }
+
+  //display answer options
+  function DisplayOptions({ copyData, ques }) {
+    const btn = copyData[ques].answers.map((i, index) => (
+      <button onClick={() => handleCheckAns(i.id)} key={index}>
+        {i.answer}
+      </button>
+    ));
+    return btn;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-       <h3>WELCOME QUIZ </h3>
-       <h3>Follow the instructions provided in Readme.md </h3>
-       <h3>If you can get this QUIZ working in around 3 to 5 hours , You are in very good shape </h3>
-      </header>
+        {/* User */}
+        <h4>WELCOME Smriti Shakya !! </h4>
 
-      
+
+        {/* Question and subquestion */}
+        {ques < copyData.length ? (
+          <>
+            <h1>{copyData[ques].question} </h1>
+            <h2>{copyData[ques].subQuestion}</h2>
+
+            {/*previous and next buttons */}
+            <button
+              id="btn-next"
+              disabled={handleNextDisabler(ques)}
+              onClick={() => handleNextClick(ques)}
+            >
+              Next
+            </button>
+            <button
+              id="btn-prev"
+              disabled={handlePrevDisabler(ques)}
+              onClick={() => handlePrevClick(ques)}
+            >
+              Previous
+            </button>
+
+            {/* answer options */}
+            <DisplayOptions copyData={copyData} ques={ques} />
+          </>
+          
+        ) : (
+          <>
+            <p> Score {score} </p>
+          </>
+        )}
+      </header>
     </div>
   );
 }
